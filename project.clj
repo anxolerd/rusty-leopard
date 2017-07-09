3fb3f3bfcaa012a75840931e9d9ca9af0543b32b
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-07-09 14:06:04 +0300

    Add SLF4J logging library
    Now there will be more logs. Ave Cthulhu!

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [yesql "0.5.3"]
                  [com.walmartlabs/lacinia "0.18.0"]
-                 [com.walmartlabs/lacinia-pedestal "0.2.0"]]
+                 [com.walmartlabs/lacinia-pedestal "0.2.0"]
+                 [org.slf4j/slf4j-simple "1.7.25"]]
   :plugins [[lein-environ "1.1.0"]]
   :main rusty-leopard.handler)
